aef1ef0fcb537a8e227c98d5afb5dc53e282d1a8
mvsg2 <89340753+mvsg2@users.noreply.github.com>
2022-09-07 01:38:57 +0530

    Add files via upload

---
+++ Hello_World.java
@@ -0,0 +1,7 @@
+// WHEN THE CLASS IS "PUBLIC", THE CLASS NAME HAS TO BE THE SAME AS THE FILE NAME.   <-------- VERY IMPORTANT TO REMEMBER
+
+public class Hello_World{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
